361184db55b46c11b1dfcb8c1f4fc460d467fab8
Wanjugu Irene <ireneemaina@gmail.com>
2015-12-04 15:53:46 +0300

    OnClick set on Login and Registration buttons

--- app/src/main/java/com/apps/dishi/Recommended.java
+++ app/src/main/java/com/apps/dishi/Recommended.java
@@ -1,6 +1,7 @@
 package com.apps.dishi;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -9,13 +10,56 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class Recommended extends Activity {
+public class Recommended extends Activity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_recommended);
+        setContentView(R.layout.content_recommended);
+
+        Button LoginButton = (Button)findViewById(R.id.LoginButton);
+        LoginButton.setOnClickListener(this);
+
+        // Creating on click change activities for a button
+        Button googleButton = (Button)findViewById(R.id.GoogleButton);
+        googleButton.setOnClickListener(this);
+
+        Button twitterButton = (Button)findViewById(R.id.TwitterButton);
+        twitterButton.setOnClickListener(this);
+
+        Button facebookButton = (Button)findViewById(R.id.FacebookButton);
+        facebookButton.setOnClickListener(this);
+
+    }
+
+    private void toRecommendedLayoutClick ()
+    {
+        Intent a = new Intent(Recommended.this, RecommendedLayout.class);
+        startActivity(a);
+    }
+    @Override
+    public void onClick(View v) {
+        switch (v.getId())
+        {
+            case R.id.GoogleButton:
+                toRecommendedLayoutClick();
+                break;
+
+            case R.id.TwitterButton:
+                toRecommendedLayoutClick();
+                break;
+
+            case R.id.FacebookButton:
+                toRecommendedLayoutClick();
+                break;
+
+            case R.id.LoginButton:
+                toRecommendedLayoutClick();
+                break;
+        }
 
     }
 }
--- app/src/main/java/com/apps/dishi/RecommendedLayout.java
+++ app/src/main/java/com/apps/dishi/RecommendedLayout.java
@@ -1,5 +1,6 @@
 package com.apps.dishi;
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -7,23 +8,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
-public class RecommendedLayout extends AppCompatActivity {
+public class RecommendedLayout extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_recommended_layout);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        setContentView(R.layout.content_recommended_layout);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
 }
