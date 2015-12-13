741b32f1d6e633bece2c2b5012470265e96ee159
Wanjugu Irene <ireneemaina@gmail.com>
2015-12-13 05:56:19 +0300

    Improved Layout: with Chef and User(eater) side sign ups

---
+++ app/src/main/java/com/apps/dishi/TypeOfUser.java
@@ -0,0 +1,42 @@
+package com.apps.dishi;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.View;
+import android.widget.Button;
+
+import com.apps.dishi.chefside.ChefLogin;
+
+public class TypeOfUser extends Activity implements View.OnClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.type_of_user);
+
+        Button EaterLoginButton = (Button)findViewById(R.id.eater_login);
+        EaterLoginButton.setOnClickListener(this);
+
+        Button ChefLoginButton = (Button)findViewById(R.id.chef_login);
+        ChefLoginButton.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId())
+        {
+            case R.id.eater_login:
+                Intent a = new Intent(this, UserLogin.class);
+                startActivity(a);
+                break;
+
+            case R.id.chef_login:
+                Intent b = new Intent(this, ChefLogin.class);
+                startActivity(b);
+                break;
+
+        }
+    }
+}
---
+++ app/src/main/java/com/apps/dishi/chefside/ChefLogin.java
@@ -0,0 +1,16 @@
+package com.apps.dishi.chefside;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+import com.apps.dishi.R;
+
+public class ChefLogin extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.chef_login);
+    }
+
+}
