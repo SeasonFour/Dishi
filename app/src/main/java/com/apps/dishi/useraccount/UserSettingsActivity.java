2818512650f02f3e0e2eb70452518f1fbd85043a
Wanjugu Irene <ireneemaina@gmail.com>
2015-12-15 04:49:52 +0300

    Guillotine Menu - Implemented in Main User Activity

---
+++ app/src/main/java/com/apps/dishi/useraccount/UserSettingsActivity.java
@@ -0,0 +1,16 @@
+package com.apps.dishi.useraccount;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+import com.apps.dishi.R;
+
+public class UserSettingsActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_settings);
+    }
+
+}
